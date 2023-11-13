01529dea380ddac5c807a5f02658abbb0246931a
JeongHeumChoi <andyc707@gmail.com>
2023-11-13 20:41:40 +0900

    feat: detail, create, list

--- pathfinder_server/pathfinder_app/serializers.py
+++ pathfinder_server/pathfinder_app/serializers.py
@@ -6,12 +6,64 @@ from .models import (
 )
 
 
-class DefectListCreateSerializer(serializers.ModelSerializer):
-    modifier    = serializers.ReadOnlyField(source='modifier.username')
+# class DefectListCreateSerializer(serializers.ModelSerializer):
+#     modifier    = serializers.ReadOnlyField(source='modifier.username')
 
+#     class Meta:
+#         model = Defect
+#         fields = [
+#             'ai_model',
+#             'modifier',
+#             'defect_type',
+#             'xmin',
+#             'ymin',
+#             'xmax',
+#             'ymax',
+#         ]
+
+
+class AiModelCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AiModel
+        fields = [
+            'rt_image',
+            'ai_model_name',
+            'score',
+            'expert_check',
+        ]
+
+    def validate_ai_model_name(self, value):
+        valid_ai_model_names = [
+            'EfficientDet',
+            'RetinaNet',
+            'Faster_R-CNN',
+            'Cascade_R-CNN',
+        ]
+        if value not in valid_ai_model_names:
+            raise serializers.ValidationError("This is not a valid AI model name.")
+        return value
+ 
+
+class RtImageCreateSerializer(serializers.ModelSerializer):
+    uploader    = serializers.ReadOnlyField(source='uploader.username')
+
+    class Meta:
+        model = RtImage
+        fields = [
+            'pk',
+            'uploader',
+            'upload_date',
+            'image',
+        ]
+
+
+class DefectListSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Defect
         fields = [
+            'pk',
             'ai_model',
             'modifier',
             'defect_type',
@@ -22,45 +74,46 @@ class DefectListCreateSerializer(serializers.ModelSerializer):
         ]
 
 
-class AiModelListCreateSerializer(serializers.ModelSerializer):
-    defect_set  = DefectListCreateSerializer(many=True)
-
+class AiModelListSerializer(serializers.ModelSerializer):
+    defect_set = DefectListSerializer(many=True)
+    
     class Meta:
         model = AiModel
         fields = [
+            'pk',
             'rt_image',
             'ai_model_name',
             'score',
             'expert_check',
+            'defect_set'
         ]
- 
 
-# View에서 보여주고, 생성할 수 있도록 하는 serializer
-class RtImageListCreateSerializer(serializers.ModelSerializer):
-    ai_model_set = AiModelListCreateSerializer(many=True)
-    uploader    = serializers.ReadOnlyField(source='uploader.username')
+    def to_representation(self, instance):
+        """Custom representation to handle no AiModel case."""
+        representation = super().to_representation(instance)
+        if not instance.defect_set.exists():
+            representation['defect_set'] = []
+        return representation
 
+class RtImageListSerializer(serializers.ModelSerializer):
+    ai_model_set = AiModelListSerializer(many=True)
+    
     class Meta:
         model = RtImage
         fields = [
-            'ai_model_set',
+            'pk',
+            'image',
             'uploader',
             'upload_date',
-            'image',
+            'ai_model_set',
         ]
 
-       
-        
-# class RtImageDetailSerializer(serializers.ModelSerializer):
-#     uploader    = serializers.ReadOnlyField(source='uploader.username')
-#     ai_model    = AiModelReadSerializer(many=True, read_only=True)
-
-#     class Meta:
-#         model = RtImage
-#         fields = [
-#             'image',
-#             'ai_model'
-#         ]
+    def to_representation(self, instance):
+        """Custom representation to handle no AiModel case."""
+        representation = super().to_representation(instance)
+        if not instance.ai_model_set.exists():
+            representation['ai_model_set'] = []
+        return representation
 
 
 class DefectDetailSerializer(serializers.ModelSerializer):
@@ -77,6 +130,7 @@ class DefectDetailSerializer(serializers.ModelSerializer):
             'ymax',
         ]
 
+
 class AiModelDetailSerializer(serializers.ModelSerializer):
     defect_set  = DefectDetailSerializer(many=True)
 
@@ -88,3 +142,33 @@ class AiModelDetailSerializer(serializers.ModelSerializer):
             'score',
             'expert_check',
         ]
+
+    def to_representation(self, instance):
+        """Custom representation to handle no AiModel case."""
+        representation = super().to_representation(instance)
+        if not instance.defect_set.exists():
+            representation['defect_set'] = []
+        return representation
+
+
+
+class RtImageDetailSerializer(serializers.ModelSerializer):
+    uploader    = serializers.ReadOnlyField(source='uploader.username')
+    ai_model_set    = AiModelDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = RtImage
+        fields = [
+            'pk',
+            'image',
+            'uploader',
+            'upload_date',
+            'ai_model_set'
+        ]
+    
+    def to_representation(self, instance):
+        """Custom representation to handle no AiModel case."""
+        representation = super().to_representation(instance)
+        if not instance.ai_model_set.exists():
+            representation['ai_model_set'] = []
+        return representation
