d45f80c32e54050fa56b5225e642e12061c1f0c2
JeongHeumChoi <andyc707@gmail.com>
2023-11-13 20:42:05 +0900

    feat: celery for ai model

--- pathfinder_server/pathfinder_app/tasks.py
+++ pathfinder_server/pathfinder_app/tasks.py
@@ -7,3 +7,11 @@ from pathfinder_server.celery import app
 def test_task(a: int, b: int):
     print("test Celery task : ", a + b)
     return a + b
+
+@shared_task
+def computer_vision_process_task(rt_image_id: int, model_name: str):
+    print("computer_vision_process_task")
+    print("rt_image_id : ", rt_image_id)
+    print("model_name : ", model_name)
+    # AiModel.objects.create(image_id=image_id, ...)
+    return 
