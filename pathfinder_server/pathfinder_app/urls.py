9ba0b63a359f89eb187a0d5890dfd0c6bb4434ad
JeongHeumChoi <andyc707@gmail.com>
2023-11-14 04:44:26 +0900

    refactor: api 설계 주석 작성 및 코드 정리

--- pathfinder_server/pathfinder_app/urls.py
+++ pathfinder_server/pathfinder_app/urls.py
@@ -3,14 +3,30 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     # RtImageViewSet,
     RtImageListCreateView,
-    Test
+    # RtImageDetailView,
+    DefectViewSet,
+    Test,
 )
 
-# router = DefaultRouter()
-# router.register('rt-images', RtImageViewSet)
+'''
+    /rt-images/                         # 목록 조회  GET
+    /rt-images/                         # 생성      POST
+    /rt-image/<int:pk>                  # 단일 조회  GET
+    /rt-image/<int:pk>/                 # 삭제      DELETE
+
+    /ai-models/<int:pk>/                # 수정     PATCH
+    /ai-models/<int:pk>/                # 삭제     DELETE
+
+    /defects/                           # 생성     POST
+    /defects/<int:pk>/                  # 수정     PATCH
+    /defects/<int:pk>/                  # 삭제     DELETE
+'''
+
+router = DefaultRouter()
+router.register('defects', DefectViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
-    path('rt-image/', RtImageListCreateView.as_view()),
+    path('', include(router.urls)),
+    path('rt-images/', RtImageListCreateView.as_view()),
     path('test/', Test.as_view()),
 ]
