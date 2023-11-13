be73a1620c07aec7dab780c21a0b61fd071a33be
JeongHeumChoi <andyc707@gmail.com>
2023-11-13 20:42:52 +0900

    feat: create, view url pattern

--- pathfinder_server/pathfinder_app/urls.py
+++ pathfinder_server/pathfinder_app/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    # RtImageViewSet,
+    RtImageListCreateView,
+    Test
+)
+
+# router = DefaultRouter()
+# router.register('rt-images', RtImageViewSet)
 
 urlpatterns = [
+    # path('', include(router.urls)),
+    path('rt-image/', RtImageListCreateView.as_view()),
+    path('test/', Test.as_view()),
 ]
