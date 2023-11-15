bf184deaecad710cef90495fe4a96fc8cf1a705f
JeongHeumChoi <andyc707@gmail.com>
2023-11-15 14:07:49 +0900

    feat: check ai model task status

--- pathfinder_server/pathfinder_app/views.py
+++ pathfinder_server/pathfinder_app/views.py
@@ -1,4 +1,7 @@
+from django.http import JsonResponse
+from celery.result import AsyncResult
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import (
@@ -29,13 +32,16 @@ class RtImageListCreateView(generics.ListCreateAPIView):
     permission_classes = [AllowAny,] # should be changed to IsAuthenticated
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        instance_id = response.data['pk']
+        response            = super().create(request, *args, **kwargs)
+        instance_id         = response.data['pk']
+        ai_model_task_id    = []
         for model_name in AiModelName:
-            computer_vision_process_task.delay(instance_id, model_name.value)
+           result = computer_vision_process_task.delay(instance_id, model_name.value)
+           ai_model_task_id.append({ model_name : result.id })
         return Response({
             'message': 'Processing started',
-            'task_id': instance_id,
+            'rt_image_id': instance_id,
+            'ai_model_task_id': ai_model_task_id,
         })
 
     def get_serializer_class(self):
@@ -61,6 +67,13 @@ class DefectViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['POST'])
+def get_tasks_status(request):
+    task_ids = request.data.get('task_ids')
+    statuses = {task_id: AsyncResult(task_id).status for task_id in task_ids}
+    return JsonResponse({'statuses': statuses})
 
 
 class Test(APIView):
