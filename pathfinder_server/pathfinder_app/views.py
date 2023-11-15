b18e4ca817006d9240d5e265ab089d5e34d15653
JeongHeumChoi <andyc707@gmail.com>
2023-11-16 05:18:35 +0900

    refactor: 코드 스타일 수정 및 filter 연결

--- pathfinder_server/pathfinder_app/views.py
+++ pathfinder_server/pathfinder_app/views.py
@@ -4,9 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import (
     generics,
     viewsets,
+    mixins,
     status,
 )
 from .models import (
@@ -24,12 +26,19 @@ from .tasks import (
     test_task,
     computer_vision_process_task,
 )
+from .filters import RtImageFilter
 from .enums import AiModelName
 
 
 class RtImageListCreateView(generics.ListCreateAPIView):
     queryset            = RtImage.objects.all()
     permission_classes  = [AllowAny,] # should be changed to IsAuthenticated
+
+    filter_class        = RtImageFilter
+
+    # filter_backends = [SearchFilter, OrderingFilter]
+    # search_fields = ['uploader__username', 'upload_date']
+    # ordering = ['-upload_date']
 
     def create(self, request, *args, **kwargs):
         response            = super().create(request, *args, **kwargs)
@@ -43,6 +52,9 @@ class RtImageListCreateView(generics.ListCreateAPIView):
             'rt_image_id'       : instance_id,
             'ai_model_task_id'  : ai_model_task_id,
         })
+    
+    def get_queryset(self):
+        return self.queryset.order_by('-upload_date')
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -57,22 +69,22 @@ class AiModelUpdateView(generics.UpdateAPIView):
     permission_classes  = [AllowAny,] # should be changed to IsAuthenticated
 
 
-class DefectViewSet(viewsets.ModelViewSet):
+class DefectViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin):
     queryset            = Defect.objects.all()
     serializer_class    = DefectSerializer
     permission_classes  = [AllowAny,] # should be changed to IsAuthenticated
-
-    def list(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def retrieve(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 @api_view(['POST'])
 def get_tasks_status(request):
     task_ids    = request.data.get('task_ids')
-    statuses    = {task_id: AsyncResult(task_id).status for task_id in task_ids}
+    statuses    = {
+        task_id: AsyncResult(task_id).status for task_id in task_ids
+    }
     return JsonResponse({'statuses': statuses})
 
 
