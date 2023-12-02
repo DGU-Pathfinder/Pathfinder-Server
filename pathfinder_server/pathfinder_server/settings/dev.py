742d892b19e8a4e47f092f5c35a6615b89e9b7c5
JeongHeumChoi <andyc707@gmail.com>
2023-12-02 23:57:55 +0900

    fix: JWT 인증 이슈 해결

--- pathfinder_server/pathfinder_server/settings/dev.py
+++ pathfinder_server/pathfinder_server/settings/dev.py
@@ -1,4 +1,5 @@
 from .common import *
+from corsheaders.defaults import default_headers
 
 INSTALLED_APPS += [
     "debug_toolbar",
@@ -8,7 +9,22 @@ MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000", "http://127.0.0.1:3000"]
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    # "127.0.0.1:3000"
+]
+
+CORS_ALLOW_HEADERS = [
+    *default_headers,
+    'Access-Control-Allow-Origin',
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods',
+    'withCredentials',
+]
+
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CELERY_BROKER_URL = get_env_variable("CELERY_BROKER_URL")
