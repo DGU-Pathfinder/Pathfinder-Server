e87a4da31f0b7aaa781aec166e3b1e919523c4a7
JeongHeumChoi <andyc707@gmail.com>
2023-12-03 20:55:05 +0900

    fix: ExpertDefectView의 create단 수정

--- pathfinder_server/pathfinder_app/views.py
+++ pathfinder_server/pathfinder_app/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from celery.result import AsyncResult
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -6,8 +7,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 # from rest_framework.filters import (
-    # SearchFilter,
-    # OrderingFilter,
+#     SearchFilter,
+#     OrderingFilter,
 # )
 from rest_framework import (
     generics,
@@ -67,6 +68,30 @@ class ExpertDefectViewSet(
 ):
     queryset            = ExpertDefect.objects.all()
     serializer_class    = ExpertDefectSerializer
+
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        rt_image = get_object_or_404(RtImage, pk=request.data['rt_image_id'])
+        data = request.data.copy()
+        try:
+            print(rt_image.expert)
+            data['expert'] = rt_image.expert.pk
+        except RtImage.expert.RelatedObjectDoesNotExist:
+            expert = Expert.objects.create(rt_image=rt_image)
+            data['expert'] = expert.pk
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(modifier=self.request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        # if not rt_image.expert:
+        #     expert = Expert.objects.create(rt_image=rt_image.pk)
+        #     data = request.data.copy()
+        #     data['expert'] = expert.pk
+        #     serializer = self.get_serializer(data=data)
+        # else:
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save(modifier=self.request.user)
 
 
 @api_view(['POST'])
