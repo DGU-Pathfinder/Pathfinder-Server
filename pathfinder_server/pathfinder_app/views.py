b269a5d9943bf5c012f8c7fcbb36ad746b1faba7
JeongHeumChoi <andyc707@gmail.com>
2023-12-08 21:48:25 +0900

    refactor: 필요없는 코드 삭제

--- pathfinder_server/pathfinder_app/views.py
+++ pathfinder_server/pathfinder_app/views.py
@@ -1,10 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from celery.result import AsyncResult
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 # from rest_framework.filters import (
 #     SearchFilter,
