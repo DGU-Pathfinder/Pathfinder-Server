d3d5f28a22f8921ab80241ab7f43f97f95f4d811
JeongHeumChoi <andyc707@gmail.com>
2023-12-08 21:47:36 +0900

    refactor: 필요없는 코드 삭제

--- pathfinder_server/accounts/models.py
+++ pathfinder_server/accounts/models.py
@@ -2,5 +2,4 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    is_active = models.BooleanField(default=True)
-    pass
+    is_active = models.BooleanField(default=False)
